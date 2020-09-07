4363f8c23331e3dcf5fc51178a4f29f89f3d9830
hansejung <sjung7.naver.com>
2020-09-07 23:26:50 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -21,37 +21,56 @@ const styles = makeStyles({
   }
 })
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '철수',
-  'birthday': '980125',
-  'gender': '남자',
-  'job': '프로그래머'  
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '순이',
-  'birthday': '980514',
-  'gender': '여자',
-  'job': '디자이너'  
-}]
- function App(){
-//class App extends Component {
-//  render(){
-// const { classes } = this.props; 
 
 
-  const classes = styles();
+
+// const customers = [{
+//   'id': 1,
+//   'image': 'https://placeimg.com/64/64/1',
+//   'name': '홍길동',
+//   'birthday': '961222',
+//   'gender': '남자',
+//   'job': '대학생'
+// },
+// {
+//   'id': 2,
+//   'image': 'https://placeimg.com/64/64/2',
+//   'name': '철수',
+//   'birthday': '980125',
+//   'gender': '남자',
+//   'job': '프로그래머'  
+// },
+// {
+//   'id': 3,
+//   'image': 'https://placeimg.com/64/64/3',
+//   'name': '순이',
+//   'birthday': '980514',
+//   'gender': '여자',
+//   'job': '디자이너'  
+// }]
+// function App(){
+class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body; 
+  }
+
+  render(){
+  const { classes } = this.props; 
+
+//  const classes = styles();
   return (
     <Paper className={classes.root}>
     <Table className={classes.table}>
@@ -66,13 +85,15 @@ const customers = [{
         </TableRow>
       </TableHead>
       <TableBody>
+
       {
-      customers.map(c =>{
-        return(
-          <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-        )        
-      }) 
-      }       
+        this.state.customers ? 
+          this.state.customers.map(c =>{
+            return(
+              <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+            )        
+          }) : ""
+      }        
       </TableBody>
       </Table>  
 
@@ -106,7 +127,7 @@ const customers = [{
       <h2> Let's develop management system!</h2> */}
     </Paper>
   );
-//    }
+  }
 }
 
 export default withStyles(styles)(App);
